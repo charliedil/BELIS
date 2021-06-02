ba6f80914ef35f3f70daad53c0f3c6c6b908f2e8
nehadil <nehadil2019@gmail.com>
2021-06-01 20:34:04 -0400

    Issue matching subwords with words. Made a number of changes trying to fix this. Still having issues. This time, however, it's hard to tell what is causing the main issue. Will have to step through with debugger until it's clear. Lots of print statements, beware.

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,6 @@
 import os
+import pickle
+
 import spacy
 from transformers import AutoTokenizer, AutoModel
 def tokenize(path):
@@ -28,7 +30,49 @@ def tokenize(path):
 
         tokenizer = AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
         model = AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
-        print(spacy_tok_sents[0])
-        bert_tokens = tokenizer(spacy_tok_sents[0],padding=True, truncation=True, max_length=512, return_tensors="pt", is_split_into_words=True, return_offsets_mapping=True)
-        print(bert_tokens.offset_mapping)
+        bert_tokens = tokenizer(spacy_tok_sents,padding=True, truncation=True, max_length=512, return_tensors="pt", is_split_into_words=True, return_offsets_mapping=True)
+        print(bert_tokens)
 
+        doc.user_data = {}
+        doc.user_data["subwords"] = []
+        i=1
+        j=0
+        # for more than one doc, for doc in docs?
+        subwords = []
+        first = True
+        for token in doc:
+            if int(bert_tokens.input_ids[j][i]) == 102:
+                i = 1
+                j += 1
+            if token.text.lower().startswith(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))):
+                if first == False:
+                    doc.user_data["subwords"].append(subwords)
+                    subwords=[]
+                subwords.append(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])))
+            elif tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])).startswith("##"):
+                subwords.append(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])))
+                i+=1
+                while(token.text.lower().startswith(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])))==False and tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])).startswith("##")):
+                    subwords.append(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])))
+                    i+=1
+                if token.text.lower().startswith(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))):
+                    if first == False:
+                        doc.user_data["subwords"].append(subwords)
+                        subwords = []
+                    subwords.append(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])))
+                else:
+                    doc.user_data["subwords"].append(subwords)
+                    subwords = []
+
+                    i -= 1
+            else:
+                doc.user_data["subwords"].append(subwords)
+                subwords = []
+
+                i-=1
+            i+=1
+            first = False
+        i=0
+        for token in doc:
+            print("Word: "+token.text+"\tSubwords: "+str(doc.user_data["subwords"][i]))
+            i+=1
