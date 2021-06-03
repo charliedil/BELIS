b2f17ac6b899a9c7cc9043cccb73d667b46ce794
charliedil <charliedil>
2021-06-02 21:35:02 -0400

    Number of fixes made to adjust mappings so they align. Most of my problems had to do with the sep token. Ended up just adding a lot of extra checks for extraneous conditions. Will need to go back and make this cleaner. But for now it gets the job done on this document, so I don't care.

--- preprocess.py
+++ preprocess.py
@@ -27,7 +27,8 @@ def tokenize(path):
             else:
                 if token.is_space == False:
                     sent.append(token.text)
-
+        if len(sent)!=0:
+            spacy_tok_sents.append(sent)
         tokenizer = AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
         model = AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
         bert_tokens = tokenizer(spacy_tok_sents,padding=True, truncation=True, max_length=512, return_tensors="pt", is_split_into_words=True, return_offsets_mapping=True)
@@ -41,12 +42,22 @@ def tokenize(path):
         subwords = []
         first = True
         finished = True
+        prev = ""
+        tcounter = 0
+        print(bert_tokens.input_ids.size())
+
         for token in doc:
             bert_token = tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))
-            if int(bert_tokens.input_ids[j][i]) == 102:
+            if token.text.lower().startswith("1826"):
+                print("howdy")
+            while (j<bert_tokens.input_ids.size()[0] and int(bert_tokens.input_ids[j][i]) == 102):
                 i = 1
                 j += 1
-            if token.text.lower().startswith(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))):
+                #bert_token = tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))
+            if j==bert_tokens.input_ids.size()[0]:
+                doc.user_data["subwords"].append(subwords)
+                break
+            if token.text.lower().startswith(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))) and finished==True:
                 if token.text.lower() != tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])).lower():
                     finished=False
                 else:
@@ -58,15 +69,23 @@ def tokenize(path):
             elif tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])).startswith("##") or finished==False:
                 subwords.append(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])))
                 i+=1
+                bert_token = tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))
                 temp = ""+subwords[0].lower() +subwords[1].lower()
+                if temp.replace("##","").lower()==prev.lower():
+                    finished = True
                 while(token.text.lower().startswith(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])))==False and (tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])).startswith("##") or finished==False)):
                     subwords.append(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])))
                     i+=1
+                    bert_token = tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))
                     temp+=subwords[len(subwords)-1].lower()
-                    if token.text.lower() == temp:
+                    print(temp.lower().replace("##",""))
+                    if prev.lower() == temp or prev.lower()==temp.lower().replace("##",""):
                         finished=True
                 finished=True
-
+                while (j<bert_tokens.input_ids.size()[0] and int(bert_tokens.input_ids[j][i]) == 102):
+                    i = 1
+                    j += 1
+                    bert_token = tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))
                 if token.text.lower().startswith(tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))):
                     if token.text.lower() != tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i])).lower():
                         finished = False
@@ -81,13 +100,24 @@ def tokenize(path):
                     subwords = []
 
                     i -= 1
+                    bert_token = tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))
             else:
                 doc.user_data["subwords"].append(subwords)
                 subwords = []
                 finished=True
                 i-=1
+                bert_token = tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))
             i+=1
+            bert_token = tokenizer.convert_ids_to_tokens(int(bert_tokens.input_ids[j][i]))
             first = False
+            prev = token.text
+        diff = len(doc) - len(doc.user_data["subwords"])
+        for i in range(len(doc)-diff, len(doc)):
+            doc.user_data["subwords"].append([])
+        # print(len(doc))
+        # print("here they are:"+doc[4611].text+doc[4612].text+doc[4613].text)
+        # print(len(doc.user_data["subwords"]))
+        # exit()
         i=0
         for token in doc:
             print("Word: "+token.text+"\tSubwords: "+str(doc.user_data["subwords"][i]))
