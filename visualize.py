f302600d676dc9e20d1b6cd9c69f90c80b0885ed
charliedil <charliedil>
2021-06-11 03:10:07 -0400

    Figured out how to visualize the subword embeddings with umap!! It is color-coded according to label. Most of it is hard-coded, which will need to be fixed in the future. Right now I do not care.

--- visualize.py
+++ visualize.py
@@ -1,23 +1,53 @@
 import numpy as np
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
-import umap
+import pandas as pd
 import spacy
 from spacy.tokens import DocBin
 from spacy.vocab import Vocab
+import umap
+
+def create_mapping(doc):
+    mapping = {}
+    mapping["Drug"] = []
+    mapping["Dosage"] = []
+    mapping["Duration"] = []
+    mapping["Frequency"] = []
+    mapping["ADE"] = []
+    mapping["Form"] = []
+    mapping["Reason"] = []
+    mapping["Strength"] = []
+    mapping["Route"] = []
+    for j in range(len(doc.user_data["subword_embeddings"])):
+        for i in range(len(doc.user_data["ents"][j])):
+            if(len(doc.user_data["ents"][j])!=1):
+                mapping[doc.user_data["ents"][j][i].split("-")[1]].append(doc.user_data["subword_embeddings"][j][i])
+    return mapping
+
 
 def draw(path):
     doc_bin = DocBin().from_disk(path)
     vocab = Vocab().from_disk("C:/Users/nehav/Desktop/n2c2_100035_vocab.spacy")
     docs = list(doc_bin.get_docs(vocab))
-    embeddings = docs[0].user_data["subword_embeddings"]
-
-
+    entity_embeddings = create_mapping(docs[0])
+    for key in entity_embeddings:
+        print(key+": "+str(len(entity_embeddings[key])))
     sns.set(style='white', rc={'figure.figsize':(12,8)})
-    np.random.seed(42)
-    data = np.random.rand(800, 4)
-    fit = umap.UMAP()
-    u = fit.fit_transform(data)
-    plt.scatter(u[:,0], u[:,1], c=data)
+
+ #   print(data)
+    i=0
+    fit = umap.UMAP(n_neighbors=5)
+
+    colors = ["palevioletred", "plum", "mediumpurple", "cornflowerblue", "skyblue", "mediumseagreen", "navajowheat", "darksalmon", "silver"]
+    for label in ["Duration","Drug", "Reason", "ADE", "Form", "Route"]:
+        print(label)
+
+        u = fit.fit_transform(entity_embeddings[label])
+        plt.scatter(u[:, 0], u[:, 1], color=colors[i], label = label)
+        i+=1
+
+    plt.legend()
     plt.show()
